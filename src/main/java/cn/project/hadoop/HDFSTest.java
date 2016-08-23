ab4269b9a30996c2017c9f6c104516ea3c611f95
StephenLU0422 <1029252135@qq.com>
2016-08-23 12:20:21 +0800

    HDFSTest can read the remote HDFS　file

--- src/main/java/cn/project/hadoop/HDFSTest.java
+++ src/main/java/cn/project/hadoop/HDFSTest.java
@@ -1,5 +1,25 @@
 package cn.project.hadoop;
 
-public class HDFSTest {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+public class HDFSTest {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		//get() require the object通过get方法获得对象
+		FileSystem fileSystem = FileSystem.get(new URI("hdfs://192.168.43.98:9000"), new Configuration());
+		//读方法,获得输入流可读取文件内容
+		FSDataInputStream fsis = fileSystem.open(new Path("/user/helloworld.txt"));
+		//写出到命令行,用工具类
+		IOUtils.copyBytes(fsis, System.out, 1024, true);
+		//关闭流
+		IOUtils.closeStream(fsis);
+		
+	}
 }
