400815324a005675915e53afc3242bb4bd2df06e
StephenLU0422 <1029252135@qq.com>
2016-08-29 13:45:08 +0800

    WordCount finish main()

--- src/main/java/cn/project/mapreduce/WordCountApp.java
+++ src/main/java/cn/project/mapreduce/WordCountApp.java
@@ -2,16 +2,62 @@ package cn.project.mapreduce;
 
 import java.io.IOException;
 
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.MapFile.Reader;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCountApp {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, WordCountApp.class.getSimpleName());
+		//打成jar包
+		job.setJarByClass(WordCountApp.class);
+		//数据是框架去做的，提供了一个类FileInputFormat，它可以指定 我们的数据，后面的路径用Path对象进行表示，端口是9000下面的hello文件，找到后委托它的子类TextInputFormat去处理
+		FileInputFormat.setInputPaths(job, new Path("hdfs://192.168.43.98:9000/helloworld.txt"));
+		job.setInputFormatClass(TextInputFormat.class);
 		
+		//将mapper和reduce告诉job
+		job.setMapperClass(MyMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
+		
+		//从mapper那里读取数据，框架自己去做的，告诉mapper函数是谁，输出类是什么，Reducer的输出类是，就是整个输出类，输出类setOutputKeyClass，它k3是Text类型，value是Longwritable类型，
+		job.setReducerClass(MyReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		//out1目录如果已经存在就会报错
+		//将数据写到那里，FileOutputFormat用来设置输出的目录out1（还不存在），谁去写
+		//我去写
+		String OUT_DIR = "hdfs://192.168.43.98:9000/out1";
+		FileOutputFormat.setOutputPath(job, new Path(OUT_DIR));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		deleteOutDir(conf,OUT_DIR);
 	}
+	
+	private static void deleteOutDir(Configuration conf, String OUT_DIR) throws IOException, URISyntaxException {
+		FileSystem fileSystem = FileSystem.get(new URI(OUT_DIR), conf);
+		//判断输出路径有没有存在OUT的目录，如果存在就delete,是（true）递归删除
+		if (fileSystem.exists(new Path(OUT_DIR))) {
+			//(path,recursive)
+			fileSystem.delete(new Path(OUT_DIR), true);
+		}
+	}
+	
 	//mapper&reducer
 	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable>{
 		//泛型，hadoop对应的是Longwritable（起始位置偏移量），Text（内容），<k1,v1>是<0, hello	you>,<10,hello me>
